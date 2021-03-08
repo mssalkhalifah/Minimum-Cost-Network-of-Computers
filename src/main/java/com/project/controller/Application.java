7b35e3420d7ac4c4b701073704074ac3eebc55a8
mssalkhalifah <mohammad.alkhalifah121@gmail.com>
2021-03-08 17:00:59 +0300

    Added EntryPoint and Application class

---
+++ src/main/java/com/project/controller/Application.java
@@ -0,0 +1,9 @@
+package com.project.controller;
+
+public class Application implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/project/controller/EntryPoint.java
@@ -0,0 +1,9 @@
+package com.project.controller;
+
+import javax.swing.*;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Application());
+    }
+}
